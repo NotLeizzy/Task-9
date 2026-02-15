a988093404baee7044a654c50405437e08430671
NotLeizzy <liwayfernandez11@gmail.com>
2026-02-15 16:13:39 +0800

    commit

--- JSWebDev.js
+++ JSWebDev.js
@@ -38,4 +38,22 @@ const levelUpBtn = document.getElementById("levelUpBtn");
     });
   }
 
+  //JS for L6
+ let coins = 0;
+
+const coinsSpan = document.getElementById("coins");
+const plusBtn = document.getElementById("plusBtn");
+const minusBtn = document.getElementById("minusBtn");
+
+plusBtn.addEventListener("click", function () {
+  coins++;
+  coinsSpan.textContent = coins;
+});
+
+minusBtn.addEventListener("click", function () {
+  if (coins > 0) {
+    coins--;
+    coinsSpan.textContent = coins;
+  }
+});
 });
